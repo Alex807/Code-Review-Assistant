3cc7e2d08f8c1dba80e19fbbcd88a4bbee84b5f6
Alexandru <Burada Alexandru>
2025-11-01 11:40:10 +0200

    set-up the pom.xml and designed app

---
+++ backend/src/test/java/com/haufegroup/hackthon/HackthonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haufegroup.hackthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackthonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
